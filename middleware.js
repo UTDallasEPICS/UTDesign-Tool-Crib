4a15843051de892a4e89039b54e1f42d342876dd
MStolen <max@amaxsteele.com>
2023-08-29 23:30:46 -0500

    Added middleware to protect admin only routes

--- middleware.js
+++ middleware.js
@@ -5,18 +5,28 @@ import { withMiddlewareAuthRequired , getSession} from "@auth0/nextjs-auth0/edge
 
 
 export default withMiddlewareAuthRequired(async function middleware(req) {
-    const res = NextResponse.next();
-    const user = await getSession(req, res);
+    const user = await getSession(req);
     // console.log(user.user["http://localhost:3000/roles"][0])
-    if (user.user["http://localhost:3000/roles"][0] === undefined) {
-        res.cookies.set('isAdmin', false)
-        // console.log("NO ADMIN")
-    } else {
-        res.cookies.set('isAdmin', true)
-        // console.log("ADMIN")
-    }
+    // if (user.user["http://localhost:3000/roles"][0] === undefined) {
+    //     res.cookies.set('isAdmin', false)
+    //     // console.log("NO ADMIN")
+    // } else {
+    //     res.cookies.set('isAdmin', true)
+    //     // console.log("ADMIN")
+    // }
     // res.cookies.set('hl', user.language);
-    return res;
+    
+    if (user.user["http://localhost:3000/roles"].includes('Admin')) {
+        if (req.nextUrl.pathname.startsWith('/Admin')) {
+            const url = req.nextUrl.clone()
+            url.pathname = '/'
+            return NextResponse.redirect(url)
+        }
+        if (req.nextUrl.pathname.startsWith('/api/admin')) {
+            return new Response("Unauthorized", {status:  401});
+        }
+    }
+    return NextResponse.next();
   });
 
 // export const config = {
