6fba48557d12f599143143d50293de1c72dc01ee
MStolen <max@amaxsteele.com>
2024-01-11 18:34:04 -0600

    Added Team Reactivation\nTeam now can be added if their team number is added again. Instead of an error, team members will be replaced

--- app/api/admin/teams/route.js
+++ app/api/admin/teams/route.js
@@ -21,7 +21,7 @@ export async function GET() {
 
 export async function PATCH(request) {
   const { team } = await request.json();
-
+  console.log(team.removeMembers);
   const studentRemoveRes = await prisma.student.updateMany({
     where: {
       id: {
@@ -62,23 +62,74 @@ export async function PATCH(request) {
 
 export async function POST(request) {
   const { team } = await request.json();
-  const teamRes = await prisma.team.create({
-    data: {
-      teamNumber: team.teamNumber,
-      tableNumber: team.tableNumber,
-      tokens: team.tokens,
-    },
+  // console.log(team);
+  const teamExists = await prisma.team.findFirst({
+    where: { teamNumber: team.teamNumber },
+    include: { teamMembers: { select: { id: true } } },
   });
-  let teamMembers = [];
-  team.teamMembers.forEach((student) => {
-    teamMembers.push({
-      name: student,
-      teamId: teamRes.id,
+  // console.log(teamExists);
+  let teamRes;
+  if (teamExists) {
+    let teamMembers = [];
+    team.teamMembers.forEach((student) => {
+      teamMembers.push({
+        name: student,
+        teamId: teamExists.id,
+      });
     });
-  });
-  const studentRes = await prisma.student.createMany({
-    data: teamMembers,
-  });
+    const studentRes = await prisma.student.createMany({
+      data: teamMembers,
+    });
+    // console.log(teamExists.teamMembers);
+    teamRes = await prisma.team.update({
+      where: {
+        id: teamExists.id,
+      },
+      data: {
+        isActive: true,
+        tableNumber: team.tableNumber,
+        teamMembers: {
+          disconnect: teamExists.teamMembers,
+        },
+      },
+    });
+
+    // console.log(teamRes);
+    // console.log(teamExists);
+    // console.log(teamExists.teamMembers);
+    const removeIds = teamExists.teamMembers.map((e) => e.id);
+    // console.log(removeIds);
+    // Remove Current students
+    // const studentRemoveRes = await prisma.student.updateMany({
+    //   where: {
+    //     id: {
+    //       in: removeIds,
+    //     },
+    //   },
+    //   data: {
+    //     teamId: { set: null },
+    //   },
+    // });
+  } else {
+    teamRes = await prisma.team.create({
+      data: {
+        teamNumber: team.teamNumber,
+        tableNumber: team.tableNumber,
+        tokens: team.tokens,
+      },
+    });
+
+    let teamMembers = [];
+    team.teamMembers.forEach((student) => {
+      teamMembers.push({
+        name: student,
+        teamId: teamRes.id,
+      });
+    });
+    const studentRes = await prisma.student.createMany({
+      data: teamMembers,
+    });
+  }
   return NextResponse.json({ team });
 }
 
