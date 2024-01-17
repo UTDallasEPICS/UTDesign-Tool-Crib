760cad246640170e848809208a7a224153aa3414
MStolen <max@amaxsteele.com>
2024-01-17 15:15:52 -0600

    More changes to node update script

--- scripts/update_node.sh
+++ scripts/update_node.sh
@@ -9,14 +9,14 @@ fi
 
 # Install/update Node and npm
 source ~/.nvm/nvm.sh
-nvm install --lts=iron --latest-npm # Install LTS version 20.x.x of node
-nvm alias default lts/iron # set default to be LTS version
+nvm install --lts=iron --latest-npm --default --no-progress # Install LTS version 20.x.x of node
 
 # Install/update pnpm
 if ! command -v pnpm &> /dev/null # check if pnpm is installed
 then
   # install pnpm
   npm install -g pnpm
+  pnpm setup
 else
 # update pnpm
   pnpm add -g pnpm
