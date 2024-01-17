50fc610c898cbac86cf932f7b6efe5c5f26224c1
MStolen <max@amaxsteele.com>
2024-01-17 15:04:52 -0600

    Comments on node update script

--- scripts/update_node.sh
+++ scripts/update_node.sh
@@ -6,9 +6,13 @@ then
   echo "Install node version manager from https://github.com/nvm-sh/nvm"
   exit 1
 fi
+
+# Install/update Node and npm
 source ~/.nvm/nvm.sh
 nvm install --lts=iron --latest-npm # Install LTS version 20.x.x of node
 nvm alias default lts/iron # set default to be LTS version
+
+# Install/update pnpm
 if ! command -v pnpm &> /dev/null # check if pnpm is installed
 then
   # install pnpm
