12cb41149d553c0cc6930da0ce3f95dd092aab0a
MStolen <max@amaxsteele.com>
2024-01-17 15:00:57 -0600

    Fix nvm command check

--- scripts/update_node.sh
+++ scripts/update_node.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Check for node version manager
-if ! command -v nvm &> /dev/null
+if ! [ -d "${HOME}/.nvm/.git" ] &> /dev/null
 then
   echo "nvm not found"
   echo "Install node version manager from https://github.com/nvm-sh/nvm"
   exit 1
 fi
-nvm install --lts=iron # Install LTS version 20.x.x of node
+source ~/.nvm/nvm.sh
+nvm install --lts=iron --latest-npm # Install LTS version 20.x.x of node
 nvm alias default lts/iron # set default to be LTS version
-nvm install-latest-npm # Update npm
 if ! command -v pnpm &> /dev/null # check if pnpm is installed
 then
   # install pnpm
